45380b81852f2e93ba32d0db05ac1ad5701dca39
Quilla Allen <1stladyofkrypton@gmail.com>
2016-10-31 16:38:20 -0400

    Testing

---
+++ MovieRental/MovieRental/Views/Home/OverdueController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRental.Views.Home
+{
+    public class OverdueController : Controller
+    {
+        // GET: Overdue
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieRental/MovieRental/Views/Home/SysAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRental.Views.Home
+{
+    public class SysAdminController : Controller
+    {
+        // GET: SysAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
